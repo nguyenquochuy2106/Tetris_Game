b6ee4cde3a0c8c76109b3f999991f54b6b6d8096
lujmin <139923103+lujmin@users.noreply.github.com>
2025-12-21 12:40:38 +0700

    Update menu.py
    Request update

--- src/ui/menu.py
+++ src/ui/menu.py
@@ -13,6 +13,16 @@ class Menu:
             self.title_font = pygame.font.SysFont("Arial", 48, bold=True)
             self.font = pygame.font.SysFont("Arial", 28)
             self.small_font = pygame.font.SysFont("Arial", 22)
+
+            # Thông tin nhóm
+        self.group_name = "Nhóm Tetris Neon"
+        self.members = [
+            {"mssv": "22730075", "name": "Nguyễn Quốc Huy"},
+            {"mssv": "22730094 ", "name": "Trần Hữu Tài"},
+            {"mssv": "25730007", "name": "Đồng Nguyễn Vũ Anh"},
+            {"mssv": "25730009", "name": "Phạm Anh"},
+            {"mssv": "22730083", "name": "Lục Gia Mẫn"},
     def main_menu(self):
         clock = pygame.time.Clock()
         width, height = self.screen.get_size()
+
